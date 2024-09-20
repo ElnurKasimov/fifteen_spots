e72a2e19d5da7cbcbfa90ceabd1f7c669719ad68
Elnur Kasimov <ekasimov2012@gmail.com>
2024-09-20 16:36:40 +0300

    created the project

---
+++ app/src/main/java/com/example/emptyproject/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.emptyproject.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF7B70A4)
+val PurpleGrey80 = Color(0xFF6E6783)
+val Pink80 = Color(0xFFD07A91)
+
+val Purple40 = Color(0xFF6244D2)
+val PurpleGrey40 = Color(0xFF7F6BA1)
+val Pink40 = Color(0xFFF1A1BE)
